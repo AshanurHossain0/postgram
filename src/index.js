92e247cf74107047e1d446a5b83a4a184a9c1762
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-04-02 16:54:55 +0530

    done-all

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const multer=require("multer")
+const mongoose=require("mongoose");
+const express=require("express");
+const app=express();
+const route=require("./routes/route");
+const cors=require("cors")
+
+app.use(express.json())
+
+app.use(cors())
+
+app.use(multer().any());
+
+mongoose.connect("mongodb+srv://ashanur:nurasha2000@ashanurdb.x6brlcb.mongodb.net/PostApp").then(()=>console.log("MongoDB is connected")).catch(err=>console.error(err))
+
+app.use("/",route);
+
+app.listen(3001, ()=>console.log(`Express app running on port ${3001}`))
